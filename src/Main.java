3166f0e36c71713eb9e3ad41156c821157eea966
g3rion <gerard.kulawik@gmail.com>
2024-05-04 11:15:40 +0200

    second commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import company.beans.Person;
+import company.exceptions.InvalidAgeException;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Person person = new Person("Mia Khalifa", 31);
+            System.out.println("Name: " + person.getName());
+            System.out.println("Age: " + person.getAge());
+        } catch (InvalidAgeException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
